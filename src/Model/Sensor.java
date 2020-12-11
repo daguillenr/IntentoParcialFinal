b209ae58ce730114a10078c38bc22d9d455d1270
Daniel Alejandro Guillén Rodríguez <daguillenr@unal.edu.co>
2020-12-11 00:31:58 -0500

    Parcial final

---
+++ src/Model/Sensor.java
@@ -0,0 +1,15 @@
+
+package Model;
+
+/**
+ *
+ * @author user
+ */
+public class Sensor {
+    
+    private int id_sensor;
+
+    private String tipo, nombre;
+    private int min_permitido, max_permitido, num_horas;
+    private boolean promedio;
+}
